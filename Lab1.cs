c0dcbcceda2aa89651ef37ec8a6f79b1b47e3983
samantha <33375727+samanthamazzola@users.noreply.github.com>
2018-06-20 16:46:27 -0400

    Update Lab1.cs
    updated version

--- Lab1.cs
+++ Lab1.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LabPart1
 {
@@ -10,33 +6,44 @@ namespace LabPart1
     {
         static void Main(string[] args)
         {
-
+            //string input
             String num1 = "";
             String num2 = "";
 
             do
             {
+
                 Console.Write("Enter first number: ");
                 num1 = Console.ReadLine();
+
+
 
                 Console.Write("Enter second number: ");
                 num2 = Console.ReadLine();
 
+
                 if (num1.Length != num2.Length)
                 {
                     Console.WriteLine("Error: number of digits do not match\nTry again\n");
+                    //  \n is like in line styling and gives you a new line
                 }
+            } 
 
-            } while (num1.Length != num2.Length);
+            while (num1.Length != num2.Length);
+
 
             int sum1 = sumOfDigits(Int32.Parse(num1));
+            //will pass sum1
             int sum2 = sumOfDigits(Int32.Parse(num2));
+
             //Console.WriteLine("sum1 is " + sum1);
             //Console.WriteLine("sum2 is " + sum2);
 
             if (sum1 != sum2)
             {
+
                 Console.WriteLine("False");
+
             }
             else
             {
@@ -47,23 +54,26 @@ namespace LabPart1
             Console.ReadKey();
         }
 
-
-
-        public static int sumOfDigits(int number)  //number = 123
+        public static int sumOfDigits(int number)  //exm if input number = 123
+        //create a function to reuse and then return int from a string; manipulate string as int
         {
+            //modulo opperation % returns the remainder of the divide
             int divisor = 10;
             int sum = 0;
 
+
+
             int aDigit = number % divisor;  // 123 % 10 = 3
+
             while (aDigit != 0)
             {
                 sum = sum + aDigit;
                 number = number / divisor;
                 aDigit = number % divisor;
+                //true division to return an int; 123/10 = 12.3 so number is 12
+                //loop around at the while loop
             }
-
             return sum;
         }
     }
 }
-

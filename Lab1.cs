64fc9f16092d69f9b6f422e84a87a573cf28425c
samantha <33375727+samanthamazzola@users.noreply.github.com>
2018-06-27 23:42:19 -0400

    update lab1
    removed modulo added another for loop

--- Lab1.cs
+++ Lab1.cs
@@ -7,74 +7,46 @@ namespace LabPart1
         static void Main(string[] args)
         {
             //string input
-            String num1 = "";
-            String num2 = "";
+            String numOne = "";
+            String numTwo = "";
 
-            do
+
+            Console.Write("Enter your first three digit whole number: ");
+            numOne = Console.ReadLine();
+
+
+            Console.Write("Enter your second three digit whole number: ");
+            numTwo = Console.ReadLine();
+
+
+            if(numOne.Length != numTwo.Length)
             {
+                Console.WriteLine("Error: number of digits do not match\nTry again\n");
+                //  \n is like in line styling and gives you a new line
+            }
 
-                Console.Write("Enter first number: ");
-                num1 = Console.ReadLine();
+            for (int i = 0; i < 1; i++)
+            {
+                int sumOne = Int32.Parse(numOne);
+                int sumTwo = Int32.Parse(numTwo);
 
 
-
-                Console.Write("Enter second number: ");
-                num2 = Console.ReadLine();
-
-
-                if (num1.Length != num2.Length)
+                if ((numOne[0] + numTwo[0]) == (numOne[1] + numTwo[1]) && (numOne[1] + numTwo[1]) == (numOne[2] + numTwo[2]))
                 {
-                    Console.WriteLine("Error: number of digits do not match\nTry again\n");
-                    //  \n is like in line styling and gives you a new line
+                    Console.WriteLine("TRUE! Sums were the same.");
                 }
-            } 
+                else
+                {
+                    Console.WriteLine("FALSE! Sums were not the same.");
+                }
 
-            while (num1.Length != num2.Length);
-
-
-            int sum1 = sumOfDigits(Int32.Parse(num1));
-            //will pass sum1
-            int sum2 = sumOfDigits(Int32.Parse(num2));
-
-            //Console.WriteLine("sum1 is " + sum1);
-            //Console.WriteLine("sum2 is " + sum2);
-
-            if (sum1 != sum2)
-            {
-
-                Console.WriteLine("False");
-
+                {
+                    Console.WriteLine("\nPress any key to exit. ");
+                    Console.ReadKey();
+                }
             }
-            else
-            {
-                Console.WriteLine("True");
-            }
-
-            Console.WriteLine("\nHit any key to continue...");
-            Console.ReadKey();
-        }
-
-        public static int sumOfDigits(int number)  //exm if input number = 123
-        //create a function to reuse and then return int from a string; manipulate string as int
-        {
-            //modulo opperation % returns the remainder of the divide
-            int divisor = 10;
-            int sum = 0;
-
-
-
-            int aDigit = number % divisor;  // 123 % 10 = 3
-
-            while (aDigit != 0)
-            {
-                sum = sum + aDigit;
-                number = number / divisor;
-                aDigit = number % divisor;
-                //true division to return an int; 123/10 = 12.3 so number is 12
-                //loop around at the while loop
-            }
-            return sum;
-            //need to add a iterative statment and for loop. still working on that ¯\_(ツ)_/¯
         }
     }
 }
+
+       
